8a55a80ed1ff752f5fb25e8a46481157bb6b1b2a
Muesseler <raphael.muesseler@sap.com>
2018-08-30 16:18:19 +0200

    added progress indicator when login and feedback from server request

--- src/main/java/de/raphaelmuesseler/financer/client/ui/login/LoginApplication.java
+++ src/main/java/de/raphaelmuesseler/financer/client/ui/login/LoginApplication.java
@@ -2,13 +2,14 @@ package de.raphaelmuesseler.financer.client.ui.login;
 
 import de.raphaelmuesseler.financer.client.connection.ServerRequestHandler;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 import java.util.HashMap;
@@ -18,10 +19,11 @@ import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class LoginApplication extends Application implements EventHandler<ActionEvent> {
+public class LoginApplication extends Application {
 
     private TextField emailTextField;
     private PasswordField passwordTextField;
+    private GridPane gridPane;
     private Logger logger = Logger.getLogger("LoginApplication");
     private ExecutorService executor = Executors.newCachedThreadPool();
 
@@ -33,36 +35,69 @@ public class LoginApplication extends Application implements EventHandler<Action
     }
 
     private void intialize(Stage primaryStage) {
-        GridPane gridPane = new GridPane();
-        gridPane.setAlignment(Pos.CENTER);
-        gridPane.setHgap(20);
-        gridPane.setVgap(10);
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 300, 250);
+
+        ProgressIndicator progressIndicator = new ProgressIndicator();
+        VBox vBox = new VBox(progressIndicator);
+        vBox.setAlignment(Pos.CENTER);
+        vBox.setVisible(false);
+
+        this.gridPane = new GridPane();
+        this.gridPane.setAlignment(Pos.CENTER);
+        this.gridPane.setHgap(20);
+        this.gridPane.setVgap(10);
 
         // add labels and textfields
-        final Label emailLabel = new Label("E-Mail: ");
-        gridPane.add(emailLabel, 0, 0);
+        Label errorLabel = new Label("Your credentials are incorrect!");
+        errorLabel.setTextFill(Color.web("#ff0000"));
+        errorLabel.setTextAlignment(TextAlignment.CENTER);
+        errorLabel.setVisible(false);
+        this.gridPane.add(errorLabel, 0, 0, 2, 1);
+
+        Label emailLabel = new Label("E-Mail: ");
+        this.gridPane.add(emailLabel, 0, 1);
         this.emailTextField = new TextField();
-        gridPane.add(this.emailTextField, 1, 0);
+        this.gridPane.add(this.emailTextField, 1, 1);
 
         Label passwordLabel = new Label("Password: ");
-        gridPane.add(passwordLabel, 0, 1);
+        this.gridPane.add(passwordLabel, 0, 2);
         this.passwordTextField = new PasswordField();
-        gridPane.add(this.passwordTextField, 1, 1);
+        this.gridPane.add(this.passwordTextField, 1, 2);
 
         CheckBox rememberMeCheckBox = new CheckBox("Remember Me");
-        gridPane.add(rememberMeCheckBox, 1, 2);
+        this.gridPane.add(rememberMeCheckBox, 1, 3);
 
         // add button
         Button btn = new Button();
         btn.setText("Login");
         btn.setId("login-btn");
-        btn.setOnAction(this);
-        gridPane.add(btn, 0, 3, 2, 1);
+        btn.setOnAction(event -> {
+            // setting progress indicator
+            vBox.setVisible(true);
+            gridPane.setDisable(true);
+
+            Map<String, Object> parameters = new HashMap<>();
+            parameters.put("email", this.emailTextField.getText());
+            parameters.put("password", this.passwordTextField.getText());
+            logger.log(Level.INFO, "User's credentials will be checked.");
+            executor.execute(new ServerRequestHandler("checkCredentials", parameters, result -> {
+                if ((Boolean) result.getResult()) {
+                    logger.log(Level.INFO, "User's credentials are correct.");
+                    errorLabel.setVisible(false);
+                } else {
+                    logger.log(Level.INFO, "User's credentials are incorrect.");
+                    errorLabel.setVisible(true);
+                }
+                gridPane.setDisable(false);
+                vBox.setVisible(false);
+            }));
+        });
+        this.gridPane.add(btn, 0, 4, 2, 1);
 
         // setting scene
-        StackPane root = new StackPane();
-        Scene scene = new Scene(root, 300, 250);
-        root.getChildren().add(gridPane);
+        root.getChildren().add(this.gridPane);
+        root.getChildren().add(vBox);
 
         // setting title
         primaryStage.setTitle("Financer - Login");
@@ -72,20 +107,5 @@ public class LoginApplication extends Application implements EventHandler<Action
 
         // show application
         primaryStage.show();
-    }
-
-    @Override
-    public void handle(ActionEvent event) {
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put("email", this.emailTextField.getText());
-        parameters.put("password", this.passwordTextField.getText());
-        this.logger.log(Level.INFO, "User's credentials will be checked.");
-        this.executor.execute(new ServerRequestHandler("checkCredentials", parameters, result -> {
-            if ((Boolean) result.getResult()) {
-                logger.log(Level.INFO, "User's credentials are correct.");
-            } else {
-                logger.log(Level.INFO, "User's credentials are incorrect.");
-            }
-        }));
     }
 }
