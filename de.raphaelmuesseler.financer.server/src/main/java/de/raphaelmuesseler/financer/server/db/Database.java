a4b3e50befe47850bb59727b91f62b9a70037def
Raphael Müßeler <raphael.muesseler@sap.com>
2018-10-25 14:05:30 +0200

    docs: added JavaDocs to Database class and removed warnings

--- de.raphaelmuesseler.financer.server/src/main/java/de/raphaelmuesseler/financer/server/db/Database.java
+++ de.raphaelmuesseler.financer.server/src/main/java/de/raphaelmuesseler/financer/server/db/Database.java
@@ -10,7 +10,6 @@ import org.json.JSONObject;
 import java.lang.reflect.Type;
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -36,7 +35,6 @@ public class Database {
     private static Database INSTANCE = null;
 
     public enum Table {
-        CATEGORIES("categories"),
         FIXED_TRANSACTIONS("fixed_transactions"),
         FIXED_TRANSACTIONS_AMOUNTS("fixed_transactions_amounts"),
         TRANSACTIONS("transactions"),
@@ -75,6 +73,11 @@ public class Database {
         }
     }
 
+    /**
+     * Sets the static host. This method should be called before the first call of getInstance.
+     *
+     * @param local sets the host to 'localhost' if true
+     */
     public static void setHost(boolean local) {
         Database.HOST = local ? Database.HOST_LOCAL : Database.HOST_DEPLOY;
     }
@@ -91,13 +94,18 @@ public class Database {
         return INSTANCE;
     }
 
-    public List<Object> getObject(Table tableName, Type resultType) throws SQLException {
-        return this.getObject(tableName, resultType, new HashMap<>());
-    }
-
-    public List<Object> getObject(Table tableName, Type resultType, Map<String, Object> whereParameters) throws SQLException {
+    /**
+     * Fetches data from the database and parses via the Gson library it to an object.
+     *
+     * @param table           Database table to fetch from
+     * @param resultType      Type of object, which will be returned
+     * @param whereParameters Where parameters which will be concatenated by AND
+     * @return Object with fetched data
+     * @throws SQLException thrown, when something went wrong executing the SQL statement
+     */
+    public List<Object> getObject(Table table, Type resultType, Map<String, Object> whereParameters) throws SQLException {
         Gson gson = new GsonBuilder().create();
-        JSONArray jsonArray = this.get(tableName, whereParameters);
+        JSONArray jsonArray = this.get(table, whereParameters);
         List<Object> result = new ArrayList<>();
         for (int i = 0; i < jsonArray.length(); i++) {
             JSONObject jsonObject = jsonArray.getJSONObject(i);
@@ -109,25 +117,45 @@ public class Database {
         return result;
     }
 
-    public JSONArray get(Table tableName, Map<String, Object> whereParameters) throws SQLException {
-        return this.get(tableName, whereParameters, null);
+    /**
+     * {@code orderByClause} set to null by default.
+     *
+     * @see Database#get(Table, Map, String)
+     */
+    public JSONArray get(Table table, Map<String, Object> whereParameters) throws SQLException {
+        return this.get(table, whereParameters, null);
     }
 
+    /**
+     * @param tableName       database table to fetch from
+     * @param whereParameters where parameters which will be concatenated by AND
+     * @param orderByClause   order by clause which will be appended on the sql statement
+     * @return JSONArray that contains
+     * @throws SQLException thrown, when something went wrong executing the SQL statement
+     */
     // TODO select specific fields
     // TODO escape strings
     public JSONArray get(Table tableName, Map<String, Object> whereParameters, String orderByClause) throws SQLException {
         PreparedStatement statement;
-        StringBuilder query = new StringBuilder("SELECT * FROM " + tableName.getTableName())
-                .append(this.getClause(whereParameters, "WHERE", " AND "))
-                .append(this.getOrderByClause(orderByClause));
+        String query = "SELECT * FROM " + tableName.getTableName() +
+                this.getClause(whereParameters, "WHERE", " AND ") +
+                this.getOrderByClause(orderByClause);
+
+        statement = connection.prepareStatement(query);
+        this.preparedStatement(statement, whereParameters);
 
         // execute query
-        statement = connection.prepareStatement(query.toString());
-        this.preparedStatement(statement, whereParameters);
         ResultSet result = statement.executeQuery();
         return Converter.convertResultSetIntoJSON(result);
     }
 
+    /**
+     * Inserts one data row into database.
+     *
+     * @param tableName table to insert
+     * @param values    this map contains the all the corresponding column values that will be inserted
+     * @throws SQLException thrown, when something went wrong executing the SQL statement
+     */
     public void insert(Table tableName, Map<String, Object> values) throws SQLException {
         PreparedStatement statement;
         StringBuilder query = new StringBuilder("INSERT INTO " + tableName + " ")
@@ -144,46 +172,57 @@ public class Database {
             query.append("?");
         }
         query.append(")");
-        statement = connection.prepareStatement(query.toString());
-        statement = this.preparedStatement(statement, values);
+        statement = this.preparedStatement(connection.prepareStatement(query.toString()), values);
 
         statement.execute();
     }
 
+    /**
+     * Updates one or multiple data rows that matches the where condition.
+     *
+     * @param tableName       database table to be updated
+     * @param whereParameters where condition to update only specific data rows
+     * @param values          values that will be updated
+     * @throws SQLException thrown, when something went wrong executing the SQL statement
+     */
     public void update(Table tableName, Map<String, Object> whereParameters, Map<String, Object> values) throws SQLException {
         PreparedStatement statement;
-        StringBuilder query = new StringBuilder("UPDATE " + tableName.getTableName())
-                .append(this.getClause(values, "SET", ", "))
-                .append(this.getClause(whereParameters, "WHERE", " AND "));
+        String query = "UPDATE " + tableName.getTableName() +
+                this.getClause(values, "SET", ", ") +
+                this.getClause(whereParameters, "WHERE", " AND ");
 
-        statement = connection.prepareStatement(query.toString());
+        statement = connection.prepareStatement(query);
         this.preparedStatement(statement, values);
         this.preparedStatement(statement, whereParameters, values.size() + 1);
         statement.execute();
 
     }
 
+    /**
+     * Deletes one or multiple data rows that matches the where condition.
+     *
+     * @param table           database table to obe updated
+     * @param whereParameters where condition to delete specific data rows
+     * @throws SQLException thrown, when something went wrong executing the SQL statement
+     */
     public void delete(Table table, Map<String, Object> whereParameters) throws SQLException {
         PreparedStatement statement;
-        StringBuilder query = new StringBuilder("DELETE FROM " + table.getTableName() + " ")
-                .append(this.getClause(whereParameters, "WHERE", " AND "));
-
-        statement = connection.prepareStatement(query.toString());
-        statement = this.preparedStatement(statement, whereParameters);
-
+        String query = "DELETE FROM " + table.getTableName() + " " +
+                this.getClause(whereParameters, "WHERE", " AND ");
+        statement = this.preparedStatement(connection.prepareStatement(query), whereParameters);
         statement.execute();
     }
 
-    private String getClause(Map<String, Object> values, String operation, String seperator) {
+    private String getClause(Map<String, Object> values, String operation, String separator) {
         StringBuilder whereClauseString = new StringBuilder();
         if (values.size() > 0) {
-            whereClauseString.append(" " + operation + " ");
+            whereClauseString.append(" ").append(operation).append(" ");
             boolean first = true;
             for (Map.Entry<String, Object> entry : values.entrySet()) {
                 if (first) {
                     first = false;
                 } else {
-                    whereClauseString.append(seperator);
+                    whereClauseString.append(separator);
                 }
                 whereClauseString.append(entry.getKey()).append(" = ?");
             }
@@ -195,7 +234,7 @@ public class Database {
         return this.preparedStatement(statement, values, 1);
     }
 
-        private PreparedStatement preparedStatement(PreparedStatement statement, Map<String, Object> values, int index) throws SQLException {
+    private PreparedStatement preparedStatement(PreparedStatement statement, Map<String, Object> values, int index) throws SQLException {
         for (Map.Entry<String, Object> entry : values.entrySet()) {
             statement.setObject(index, entry.getValue());
             index++;
