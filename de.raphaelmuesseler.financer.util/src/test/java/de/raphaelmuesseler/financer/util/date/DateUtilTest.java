30beb657e19a51a0a5eaaccbd7087a8397fb2d74
Raphael Müßeler <raphael.muesseler@sap.com>
2018-10-31 09:01:31 +0100

    tests: implemented test for DateUtil class

---
+++ de.raphaelmuesseler.financer.util/src/test/java/de/raphaelmuesseler/financer/util/date/DateUtilTest.java
@@ -0,0 +1,44 @@
+package de.raphaelmuesseler.financer.util.date;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+class DateUtilTest {
+    @Test
+    void testMonthDifference() {
+        LocalDate localDate1 = LocalDate.of(2018, 5, 7);
+        LocalDate localDate2 = LocalDate.of(2017, 9, 30);
+
+        Assertions.assertEquals(-8, DateUtil.getMonthDifference(localDate1, localDate2));
+
+        localDate1 = LocalDate.of(2018, 5, 7);
+        localDate2 = LocalDate.of(2018, 6, 3);
+
+        Assertions.assertEquals(1, DateUtil.getMonthDifference(localDate1, localDate2));
+
+        localDate1 = LocalDate.of(2018, 6, 7);
+        localDate2 = LocalDate.of(2018, 6, 3);
+
+        Assertions.assertEquals(0, DateUtil.getMonthDifference(localDate1, localDate2));
+    }
+
+    @Test
+    void testMonthEquality() {
+        LocalDate localDate1 = LocalDate.of(2018, 5, 7);
+        LocalDate localDate2 = LocalDate.of(2017, 9, 30);
+
+        Assertions.assertFalse(DateUtil.checkIfMonthsAreEqual(localDate1, localDate2));
+
+        localDate1 = LocalDate.of(2018, 5, 7);
+        localDate2 = LocalDate.of(2018, 6, 3);
+
+        Assertions.assertFalse(DateUtil.checkIfMonthsAreEqual(localDate1, localDate2));
+
+        localDate1 = LocalDate.of(2018, 6, 7);
+        localDate2 = LocalDate.of(2018, 6, 3);
+
+        Assertions.assertTrue(DateUtil.checkIfMonthsAreEqual(localDate1, localDate2));
+    }
+}
