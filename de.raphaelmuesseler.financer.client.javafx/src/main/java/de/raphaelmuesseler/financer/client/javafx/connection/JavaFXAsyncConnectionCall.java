c0c1cc59e969392534c5b6768ebf2600e6d556da
Raphael Müßeler <raphael.muesseler@sap.com>
2019-01-18 15:59:02 +0100

    security: implemented logging out when NotAuthorizedException is thrown

--- de.raphaelmuesseler.financer.client.javafx/src/main/java/de/raphaelmuesseler/financer/client/javafx/connection/JavaFXAsyncConnectionCall.java
+++ de.raphaelmuesseler.financer.client.javafx/src/main/java/de/raphaelmuesseler/financer/client/javafx/connection/JavaFXAsyncConnectionCall.java
@@ -2,6 +2,8 @@ package de.raphaelmuesseler.financer.client.javafx.connection;
 
 import de.raphaelmuesseler.financer.client.connection.AsyncConnectionCall;
 import de.raphaelmuesseler.financer.client.javafx.dialogs.FinancerExceptionDialog;
+import de.raphaelmuesseler.financer.client.javafx.main.FinancerController;
+import de.raphaelmuesseler.financer.shared.exceptions.NotAuthorizedException;
 import javafx.application.Platform;
 
 public interface JavaFXAsyncConnectionCall extends AsyncConnectionCall {
@@ -16,6 +18,10 @@ public interface JavaFXAsyncConnectionCall extends AsyncConnectionCall {
             FinancerExceptionDialog dialog = new FinancerExceptionDialog("Login", exception);
             dialog.showAndWait();
             runnable.run();
+
+            if (exception instanceof NotAuthorizedException) {
+                FinancerController.handleLogout();
+            }
         });
 
     }
