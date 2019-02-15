419d00734246b517d793d420f93f294d16a78a6a
Raphael Müßeler <raphael.muesseler@sap.com>
2019-02-15 09:00:09 +0100

    tests: added logging to test for finding error on server

--- de.raphaelmuesseler.financer.client.javafx/src/test/java/de/raphaelmuesseler/financer/client/javafx/main/FixedTransactionTest.java
+++ de.raphaelmuesseler.financer.client.javafx/src/test/java/de/raphaelmuesseler/financer/client/javafx/main/FixedTransactionTest.java
@@ -134,6 +134,8 @@ public class FixedTransactionTest extends AbstractFinancerApplicationTest {
 
         Assertions.assertNotNull(find((Label label) -> label.getText().contains(I18N.get("active"))));
         Assertions.assertEquals(450.0, fixedTransaction.getTransactionAmounts().get(0).getAmount());
+        System.out.println(Formatter.formatCurrency(
+                fixedTransaction.getTransactionAmounts().get(0).getAmount()));
         Assertions.assertNotNull(find((Label label) -> label.getText().contains(Formatter.formatCurrency(
                 fixedTransaction.getTransactionAmounts().get(0).getAmount()))));
     }
