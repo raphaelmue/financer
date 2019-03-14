cc09461df79943ceee520f8332d2559f06f6a2a7
Raphael Müßeler <raphael@muesseler.de>
2019-03-14 23:01:49 +0100

    deployment: fixed shell script

--- service/start-financer-server.sh
+++ service/start-financer-server.sh
@@ -2,4 +2,4 @@
 
 kill -9 $(lsof -t -i :3500)
 cd ./../de.raphaelmuesseler.financer.server/target/
-java -jar de.raphaelmuesseler.financer.server-1.0-SNAPSHOT-jar-with-dependencies.jar --database=prod
+java -jar de.raphaelmuesseler.financer.server-1.0-SNAPSHOT-jar-with-dependencies.jar --database=prod &
