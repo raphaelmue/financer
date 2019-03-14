6ddbacad8ddfaf76f2ab1a06ed697965ccac293b
Raphael Müßeler <raphael@muesseler.de>
2019-03-14 23:27:27 +0100

    deployment: improved shell script

--- service/start-financer-server.sh
+++ service/start-financer-server.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-kill -9 $(lsof -t -i :3500)
-cd ./de.raphaelmuesseler.financer.server/target/
+PID=$(lsof -t -i :3500)
+
+if [[ ! -z "$PID" && "$PID" != " " ]];
+then
+        echo 'Financer server is currently running on pid:' "$PID"
+        echo 'Killing process ...'
+        kill -9 $PID
+fi
+
+cd ./../de.raphaelmuesseler.financer.server/target/
 java -jar de.raphaelmuesseler.financer.server-1.0-SNAPSHOT-jar-with-dependencies.jar --database=prod &
+
+echo 'Restarted financer server successfully!'
