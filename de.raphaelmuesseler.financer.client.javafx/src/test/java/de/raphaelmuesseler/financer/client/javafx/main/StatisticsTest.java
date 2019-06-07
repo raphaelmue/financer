7bd0ee6fcdefdbf45095da9d676e98429c0f6197
Raphael Müßeler <raphael.muesseler@sap.com>
2019-06-07 11:12:06 +0200

    issue #54: implemented test for progress line chart

--- de.raphaelmuesseler.financer.client.javafx/src/test/java/de/raphaelmuesseler/financer/client/javafx/main/StatisticsTest.java
+++ de.raphaelmuesseler.financer.client.javafx/src/test/java/de/raphaelmuesseler/financer/client/javafx/main/StatisticsTest.java
@@ -1,15 +1,18 @@
 package de.raphaelmuesseler.financer.client.javafx.main;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDatePicker;
-import de.raphaelmuesseler.financer.client.format.I18N;
-import de.raphaelmuesseler.financer.client.javafx.format.JavaFXFormatter;
-import de.raphaelmuesseler.financer.client.javafx.local.LocalStorageImpl;
 import de.raphaelmuesseler.financer.shared.model.categories.BaseCategory;
+import de.raphaelmuesseler.financer.shared.model.categories.CategoryTree;
 import javafx.geometry.VerticalDirection;
+import javafx.scene.chart.LineChart;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
@@ -26,6 +29,36 @@ public class StatisticsTest extends AbstractFinancerApplicationTest {
     public void setUpEach() throws Exception {
         super.setUpEach();
         ApplicationTest.launch(FinancerApplication.class);
+    }
+
+    @Test
+    public void testProgressChart() {
+        register(user, password);
+        transaction.setValueDate(LocalDate.now().withDayOfMonth(1));
+        addCategory(category);
+        addTransaction(transaction);
+
+        clickOn((Button) find("#statisticsTabBtn"));
+        sleep(SHORT_SLEEP);
+
+        LineChart lineChart = find("#progressLineChart");
+        Assertions.assertEquals(1, lineChart.getData().size());
+
+        clickOn((ComboBox) find("#progressChartDefaultCategoryComboBox"));
+        press(KeyCode.DOWN).release(KeyCode.DOWN);
+        press(KeyCode.ENTER).release(KeyCode.ENTER);
+
+        clickOn((JFXButton) find("#addCategoryBtn"));
+        VBox categoriesContainer = find("#categoriesContainer");
+        HBox dropDownHBox = (HBox) categoriesContainer.getChildren().get(1);
+        ComboBox<CategoryTree> categoryComboBox = (ComboBox<CategoryTree>) dropDownHBox.getChildren().get(0);
+        clickOn(categoryComboBox);
+        press(KeyCode.DOWN).release(KeyCode.DOWN);
+        press(KeyCode.ENTER).release(KeyCode.ENTER);
+
+        sleep(SHORT_SLEEP);
+        lineChart = find("#progressLineChart");
+        Assertions.assertEquals(2, lineChart.getData().size());
     }
 
     @Test
