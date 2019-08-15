20d84090999ec93adf850b51a9848af094a85d98
Raphael Müßeler <raphael.muesseler@sap.com>
2019-08-15 16:42:35 +0200

    issue #65: added first draft for website

---
+++ web/app.js
@@ -0,0 +1,47 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const i18n = require('./src/i18nHelper');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(i18n.init);
+
+// define resource directories
+app.use(express.static(path.join(__dirname, '/public')));
+app.use("/jquery", express.static(__dirname + '/node_modules/jquery/dist'));
+app.use("/bootstrap", express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use("/aos", express.static(__dirname + '/node_modules/aos/dist'));
+
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
